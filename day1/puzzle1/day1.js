be7e95f10c015030adaf0eebbcad54cbf3e1629b
Anouar Touati <marouan1996@hotmail.fr>
2024-12-01 14:35:46 -0500

    day1

---
+++ day1/puzzle1/day1.js
@@ -0,0 +1,41 @@
+const fs = require("node:fs/promises");
+
+const input = await fs.readFile("input.txt", { encoding: "utf8" });
+
+const leftNumbers = [];
+const rightNumbers = [];
+
+input.split("\n").forEach((line) => {
+  const twoHalfsOfLine = line.split("   ");
+  leftNumbers.push(twoHalfsOfLine[0]);
+  rightNumbers.push(twoHalfsOfLine[1]);
+});
+
+async function puzzle1() {
+  leftNumbers.sort((a, b) => a - b);
+  rightNumbers.sort((a, b) => a - b);
+
+  let totalDifference = 0;
+
+  for (let i = 0; i < leftNumbers.length; i++) {
+    totalDifference += Math.abs(leftNumbers[i] - rightNumbers[i]);
+  }
+
+  console.log(totalDifference);
+}
+puzzle1();
+
+async function puzzle2() {
+  let similarityScore = 0;
+
+  for (let i = 0; i < leftNumbers.length; i++) {
+    for (let j = 0; j < leftNumbers.length; j++) {
+      if (Number(leftNumbers[i]) == Number(rightNumbers[j])) {
+        similarityScore += Number(leftNumbers[i]);
+      }
+    }
+  }
+
+  console.log(similarityScore);
+}
+puzzle2();
