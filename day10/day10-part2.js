e45151525bf8cbe14bcd7eb8d5553c666735ab3a
Anouar Touati <marouan1996@hotmail.fr>
2024-12-14 01:46:12 -0500

    day 10 part 2 done

---
+++ day10/day10-part2.js
@@ -0,0 +1,68 @@
+const fs = require("node:fs/promises");
+
+let grid = [];
+
+let rowsCount = 0;
+let columnsCount = 0;
+
+const getData = async () => {
+  let input = await fs.readFile("input.txt", { encoding: "utf8" });
+  input = input.replaceAll("\r", "").split("\n");
+  input.forEach((row) => {
+    grid.push(row.split(""));
+  });
+
+  rowsCount = grid.length;
+  columnsCount = grid[0].length;
+  for (let i = 0; i < rowsCount; i++) {
+    for (let j = 0; j < columnsCount; j++) {
+      grid[i][j] = Number(grid[i][j]);
+    }
+  }
+};
+
+const getAdjacencyList = (i, j) => {
+  const adjacencyList = [];
+  if (i + 1 < rowsCount) {
+    adjacencyList.push([Number(i) + 1, j]);
+  }
+  if (i - 1 >= 0) {
+    adjacencyList.push([Number(i) - 1, j]);
+  }
+  if (j + 1 < columnsCount) {
+    adjacencyList.push([i, Number(j) + 1]);
+  }
+  if (j - 1 >= 0) {
+    adjacencyList.push([i, Number(j) - 1]);
+  }
+  return adjacencyList;
+};
+
+let trailsCount = 0;
+function dfs(start, lastValue, depth) {
+  if (grid[start[0]][start[1]] == 9 && depth === 9) {
+    trailsCount++;
+    return;
+  } else if (depth === 9) {
+    return;
+  }
+
+  const nodes = getAdjacencyList(...start);
+  nodes.forEach((node) => {
+    if (grid[node[0]][node[1]] === lastValue + 1) {
+      dfs(node, grid[node[0]][node[1]], depth + 1);
+    }
+  });
+}
+async function puzzle() {
+  await getData();
+  grid.forEach((row, i) => {
+    row.forEach((node, j) => {
+      if (node === 0) {
+        dfs([i, j], 0, 0);
+      }
+    });
+  });
+  console.log("total valid trails is : " + trailsCount);
+}
+puzzle();
